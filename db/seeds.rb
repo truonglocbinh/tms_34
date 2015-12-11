9a2a97013bb0eadc0c85ccbc6b49bce6aeb687e8
truonglocbinh <truonglocbinh93@gmail.com>
2015-12-11 14:08:58 +0700

    view subject

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 5.times do |n|
-  name = Faker::Name.name
-  email = "supervisor-#{n+1}@framgia.com"
-  User.create!(name:  name, email: email, password: "1234567",
-    password_confirmation: "1234567", role: :supervisor)
-end
-
-50.times do |n|
+  subject = Subject.first
   name = Faker::Name.title
   description = Faker::Lorem.paragraphs(5).join(" <br /> ")
-  Subject.create!(name: name, description: description)
+  Task.create!(name: name, description: description, subject_id: subject.id)
 end
